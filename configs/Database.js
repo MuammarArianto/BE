4f97ca512a0717436558690aa3a7d83d6d4b995f
Asus <muammararianto201@gmail.com>
2023-12-11 00:12:01 +0800

    Bis

--- configs/Database.js
+++ configs/Database.js
@@ -4,7 +4,10 @@ import dotenv from "dotenv"
 dotenv.config();
 
 // Koneksi ke database menggunakan Sequelize
-const db = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
+const db = new Sequelize({
+    database: process.env.DB_NAME,
+    username: process.env.DB_USER, 
+    password: process.env.DB_PASSWORD,
     host: process.env.DB_HOST,
     port: process.env.DB_PORT,
     dialect: "mysql"
